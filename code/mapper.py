9b69293e001d44283a6368f1f763f41fa211d08b
Raynald Chung <raynaldchung@gmail.com>
2014-03-18 21:06:05 +0100

    Annotated

--- code/mapper.py
+++ code/mapper.py
@@ -9,22 +9,20 @@ from sets import Set
 r = 8  #int(sys.argv[1])
 b = 16 #int(sys.argv[2])
 hashnum = 128
-buckets = 256
-buck = [set() for index in xrange(buckets)]
-#SigMatrix = []
+buckets = 256 # number of buckets
+buck = [set() for index in xrange(buckets)] # sets of bucket
 
 def partition(video_id, shingles):
-    #pass
     key = ""
     for word in shingles:
         for i in range(0, hashnum,1):
             M[i] = min((hasha[i]*word+hashb[i])%10001, M[i])
-    #SigMatrix += [M[i]]
+    #Passing signature matrix to reducer
     print "M,",
     for i in range(0,hashnum,1):
         print int(M[i]), '\t',
     print
-
+    #Mapping to buckets
     for i in range(0, hashnum, r):
         h = 0
         for j in range(0, r, 1):
@@ -38,11 +36,12 @@ if __name__ == "__main__":
     np.random.seed(seed=42)
     hasha = []
     hashb = []
-
+    #Generate hash function paramters
     hasha = np.random.randint(1,10001, hashnum)
     hashb = np.random.randint(1,10001, hashnum)
     hashc = np.random.randint(1, buckets, r)
     hashd = np.random.randint(1, buckets, r)
+
     for line in sys.stdin:
         line = line.strip()
         video_id = int(line[6:15])
@@ -51,24 +50,11 @@ if __name__ == "__main__":
         for i in range(0, hashnum,1):
             M.append(10001)
         partition(video_id, shingles)
-
+    #Passing bucket sets to reducer
     for i in range(0, buckets ,1):
         if len(buck[i]) > 0 :
             print 'B,',
             for j in buck[i]:
                 print j, '\t',
             print
-            """
-            for j in range(0, len(buck[i]), 1):
-                id_j = int(buck[i][j])
-                for k in range(j+1, len(buck[i]), 1):
-                    id_k = int(buck[i][k])
-                    ans = 0
-                    for l in range(0, hashnum, 1):
-                        if SigMatrix[id_j][l]==SigMatrix[id_k][l]:
-                            ans = ans + 1
-                    if ans * 1.0 / hashnum >= 0.85:
-                        print "%s\t%s" % (min(id_j, id_k),
-                            max(id_j, id_k))
-                        """
 
--- code/reducer.py
+++ code/reducer.py
@@ -3,19 +3,8 @@
 import numpy as np
 import sys
 
-"""
-def print_duplicates(videos):
-    unique = np.unique(videos)
-    for i in xrange(len(unique)):
-        for j in xrange(i + 1, len(unique)):
-            print "%d\t%d" % (min(unique[i], unique[j]),
-                              max(unique[i], unique[j]))
-"""
-
-last_key = None
 key_count = 0
 buck_count = 0
-duplicates = []
 video_id = []
 keystore = []
 buckstore = []
@@ -31,15 +20,13 @@ for line in sys.stdin:
         buckstore.append(video.split("\t"))
         buck_count = buck_count + 1
 
-    #video_id.append(video)
-    #keystore.append(key.split("\t"))
-
 for i in range(0, buck_count, 1):
     for j in range(0, len(buckstore[i]), 1):
         id_j = int(buckstore[i][j])
         for k in range(j+1, len(buckstore[i]), 1):
             id_k = int(buckstore[i][k])
             ans = 0
+            #Compare signature matrix
             for l in range(0, hashnum, 1):
                 if keystore[id_j][l]==keystore[id_k][l]:
                     ans = ans + 1
@@ -48,18 +35,3 @@ for i in range(0, buck_count, 1):
                                   max(id_j, id_k))
 
 
-        """
-    if last_key is None:
-        last_key = key
-
-    if key == last_key:
-        duplicates.append(int(video_id))
-    else:
-        # Key changed (previous line was k=x, this line is k=y)
-        print_duplicates(duplicates)
-        duplicates = [int(video_id)]
-        last_key = key
-
-if len(duplicates) > 0:
-    print_duplicates(duplicates)
-    """
