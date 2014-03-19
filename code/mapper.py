41d291f12ebe50698cfb7fc4e3c5f5bccb7a96e4
Raynald Chung <raynaldchung@gmail.com>
2014-03-19 19:13:20 +0100

    Revised

--- code/mapper.py
+++ code/mapper.py
@@ -12,22 +12,18 @@ hashnum = 128
 buckets = 256 # number of buckets
 buck = [set() for index in xrange(buckets)] # sets of bucket
 
-def partition(video_id, shingles):
-    key = ""
+def partition(line):
+    video_id = int(line[6:15])
+    shingles = np.fromstring(line[16:], sep=" ")
     for word in shingles:
         for i in range(0, hashnum,1):
             M[i] = min((hasha[i]*word+hashb[i])%10001, M[i])
-    #Passing signature matrix to reducer
-    print "M,",
-    for i in range(0,hashnum,1):
-        print int(M[i]), '\t',
-    print
     #Mapping to buckets
     for i in range(0, hashnum, r):
         h = 0
         for j in range(0, r, 1):
             h = (M[i + j] * hashc[j] + hashd[j]) % buckets
-        buck[int(h)].add(video_id)
+        buck[int(h)].add(line)
 
 
 if __name__ == "__main__":
@@ -44,17 +40,13 @@ if __name__ == "__main__":
 
     for line in sys.stdin:
         line = line.strip()
-        video_id = int(line[6:15])
-        shingles = np.fromstring(line[16:], sep=" ")
         M = []
         for i in range(0, hashnum,1):
             M.append(10001)
-        partition(video_id, shingles)
+        partition(line)
     #Passing bucket sets to reducer
     for i in range(0, buckets ,1):
         if len(buck[i]) > 0 :
-            print 'B,',
             for j in buck[i]:
-                print j, '\t',
-            print
+                print i, ":", j
 
--- code/reducer.py
+++ code/reducer.py
@@ -2,36 +2,36 @@
 
 import numpy as np
 import sys
+from sets import Set
 
 key_count = 0
 buck_count = 0
 video_id = []
-keystore = []
-buckstore = []
+buckets = 256
+buck = [set() for index in xrange(buckets)]
 hashnum = 128
 
 for line in sys.stdin:
     line = line.strip()
-    key, video= line.split(",")
-    if key == 'M':
-        keystore.append(video.split("\t"))
-        key_count = key_count + 1
-    else:
-        buckstore.append(video.split("\t"))
-        buck_count = buck_count + 1
+    bucknr, ert = line.split(":")
+    buck[int(bucknr)].add(ert)
 
-for i in range(0, buck_count, 1):
-    for j in range(0, len(buckstore[i]), 1):
-        id_j = int(buckstore[i][j])
-        for k in range(j+1, len(buckstore[i]), 1):
-            id_k = int(buckstore[i][k])
-            ans = 0
-            #Compare signature matrix
-            for l in range(0, hashnum, 1):
-                if keystore[id_j][l]==keystore[id_k][l]:
-                    ans = ans + 1
-            if ans * 1.0 / hashnum >= 0.85:
-                print "%s\t%s" % (min(id_j, id_k),
-                                  max(id_j, id_k))
+for i in range(0, buckets, 1):
+    buck[i] = np.unique(buck[i])
+    for j in range(0, len(buck[i]), 1):
+        video_j = int(buck[i][j][7:16])
+        num_j= np.fromstring(buck[i][j][17:], dtype = int, sep = " ")
+        set_j=Set()
+        for x in num_j:
+            set_j.add(x)
+        for k in range(j+1, len(buck[i]), 1):
+            video_k = int(buck[i][k][7:16])
+            num_k= np.fromstring(buck[i][k][17:], dtype = int, sep = " ")
+            set_k=Set()
+            for x in num_k:
+                set_k.add(x)
+            if len(set_j.intersection(set_k))*1.0/ len(set_j.union(set_k)) >= 0.85:
+                print "%s\t%s" % (min(video_j, video_k),
+                                  max(video_j, video_k))
 
 
